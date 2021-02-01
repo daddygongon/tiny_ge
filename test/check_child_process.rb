68f1b923f2e5094c909f20af1f09d33838815665
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2021-02-01 19:30:47 +0900

    mk kill_child_process.rb

---
+++ test/check_child_process.rb
@@ -0,0 +1,31 @@
+require 'command_line/global'
+
+res = command_line('ps -xal')
+
+pid = ARGV[0] || '13931'
+data = []
+res.stdout.split("\n").each do |line|
+  data << line.match(/^(\d+)\s+(\d+)\s+(\d+)\s+(\d+)(.+)/)
+end
+
+pids = [pid]
+data.each do |item|
+  next if item==nil
+  if item[4] == pid
+    puts item[0]
+    pid = item[3]
+    pids << pid
+  end
+end
+pid = pids[-2]
+data.each do |item|
+  next if item==nil
+  if item[4] == pid
+    puts item[0]
+    pids << item[3]
+  end
+end
+
+pids.uniq.each do |pid|
+  puts "kill -9 #{pid}"
+end
