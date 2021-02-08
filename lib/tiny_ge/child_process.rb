982b9431654c1ffad301b238e8b55eea01f8838d
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2021-02-09 08:17:58 +0900

    rev qdel where I did String == Integer...

--- lib/tiny_ge/child_process.rb
+++ lib/tiny_ge/child_process.rb
@@ -4,28 +4,27 @@ module ChildProcess
   def find_child_process_recursively(pid)
     $data.each do |item|
       next if item==nil
-      if item[4] == pid
-        puts item[0]
-        $pids << item[3]
-        find_child_process_recursively(item[3])
+       if item[4].to_i == pid.to_i
+        $pids << item[3].to_i
+        find_child_process_recursively(item[3].to_i)
       end
     end
     return nil
   end
 
   def kill_all_child_process(pid)
-    #pid = ARGV[0] || '13931'
-
+    p $pids = [pid]
     # store jobs in $data
     $data = command_line('ps -xal').stdout.split("\n").inject([]) do |dd, line|
       dd << line.match(/^(\d+)\s+(\d+)\s+(\d+)\s+(\d+)(.+)/)
+      # stored in String
     end
 
-    $pids = [pid]
     find_child_process_recursively(pid)
-
+    p $pids
     $pids.uniq.each do |pid|
-      command_line "kill -9 #{pid}"
+      p command = "kill -9 #{pid}"
+      command_line command
     end
   end
 end
